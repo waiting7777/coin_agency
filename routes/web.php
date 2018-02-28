430bb29df6c2422ae3cf8aa472dac74ad2b50ac9
Waiting7777 <waiting0819@msn.com>
2018-03-01 02:54:29 +0800

    add res test

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,30 @@ Route::get('/test', function() {
 });
 
 Route::post('/webhook', function(Request $request){
-    // Log::channel('slack')->info($request->all());
-    $challenge = $request->input('challenge');    
-    return response($challenge, 200)->header('Content-Type', 'text/plain');
+    $data = $request->all();
+    if(strpos($data['event']['text'], '<@U9FAL208Y>') !== false){
+        // $data = json_decode(Curl::to('https://api.coinmarketcap.com/v1/ticker/?limit=1')->get());
+        $res = array('attachments' => 
+            array(
+                array(
+                    'fallback' => 'Data not avaiable',
+                    'color' => '#70A800',
+                    'pretext' => 'Latest price from CoinMarketCap:',
+                    'author_name' => 'Bitcoin (BTC)',
+                    'author_link' => 'https://coinmarketcap.com/currencies/bitcoin/',
+                    'author_icon' => 'https://files.coinmarketcap.com/static/img/coins/32x32/1.png',
+                    'fields' => array(
+                        array(
+                            'title' => 'Rank',
+                            'value' => '1',
+                            'short'=> true
+                        ),
+                    )
+                )
+            )
+        );
+        $response = Curl::to('https://hooks.slack.com/services/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')->withData( $res )->asJson()->post();
+        // Log::channel('slack')->info($data[0]->symbol);
+    }
+    return response('hello', 200)->header('Content-Type', 'text/plain');
 });

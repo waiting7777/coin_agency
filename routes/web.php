6587eb8c32f9fc1ad63e0ce6dbd9b4dd94ddc698
Waiting7777 <waiting0819@msn.com>
2018-03-01 03:04:16 +0800

    change to real data

--- routes/web.php
+++ routes/web.php
@@ -28,23 +28,69 @@ Route::get('/test', function() {
 Route::post('/webhook', function(Request $request){
     $data = $request->all();
     if(strpos($data['event']['text'], '<@U9FAL208Y>') !== false){
-        // $data = json_decode(Curl::to('https://api.coinmarketcap.com/v1/ticker/?limit=1')->get());
+        $data = json_decode(Curl::to('https://api.coinmarketcap.com/v1/ticker/?limit=1')->get());
         $res = array('attachments' => 
             array(
                 array(
                     'fallback' => 'Data not avaiable',
                     'color' => '#70A800',
                     'pretext' => 'Latest price from CoinMarketCap:',
-                    'author_name' => 'Bitcoin (BTC)',
+                    'author_name' => $data->name . ' (' . $data->symbol . ')',
                     'author_link' => 'https://coinmarketcap.com/currencies/bitcoin/',
                     'author_icon' => 'https://files.coinmarketcap.com/static/img/coins/32x32/1.png',
                     'fields' => array(
                         array(
                             'title' => 'Rank',
-                            'value' => '1',
+                            'value' => $data->rank,
                             'short'=> true
                         ),
-                    )
+                        array(
+                            'title' => 'Price',
+                            'value' => '$' . $data->price_usd,
+                            'short'=> true
+                        ),
+                        array(
+                            'title' => 'Available Supply',
+                            'value' => $data->available_supply . ' ' . $data->name,
+                            'short'=> true
+                        ),
+                        array(
+                            'title' => 'Total Supply',
+                            'value' => $data->total_supply . ' ' . $data->name,
+                            'short'=> true
+                        ),
+                        array(
+                            'title' => 'Max Supply',
+                            'value' => $data->max_supply . ' ' . $data->name,
+                            'short'=> true
+                        ),
+                        array(
+                            'title' => 'Volume (24h)',
+                            'value' => '$' . $data['24h_volume_usd'],
+                            'short'=> true
+                        ),
+                        array(
+                            'title' => 'Market Cap',
+                            'value' => '$' . $data->market_cap_usd,
+                            'short'=> true
+                        ),
+                        array(
+                            'title' => 'Percent Change (1h)',
+                            'value' => $data->percent_change_1h . '%',
+                            'short'=> true
+                        ),
+                        array(
+                            'title' => 'Percent Change (24h)',
+                            'value' => $data->percent_change_24h . '%',
+                            'short'=> true
+                        ),
+                        array(
+                            'title' => 'Percent Change (7d)',
+                            'value' => $data->percent_change_7d . '%',
+                            'short'=> true
+                        )
+                    ),
+                    'ts' => $data->last_updated
                 )
             )
         );
